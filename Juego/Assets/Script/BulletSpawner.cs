e59b5f9788d014407a57833d7a34af8e4d96e51c
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2021-11-15 11:12:09 -0300

    agregadas lucesen los diamantes, luces en los podios

--- Juego/Assets/Script/BulletSpawner.cs
+++ Juego/Assets/Script/BulletSpawner.cs
@@ -7,18 +7,29 @@ public class BulletSpawner : MonoBehaviour
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject player;
 
+    private float waitTime = 2.0f;
+    private float timer = 0.0f;
+    private float playerDistance = 15f;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-        InvokeRepeating("Spawner", 1f, 3f);
-
+        //   InvokeRepeating("Spawner", 1f, 3f);
+        timer = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        Vector3 distancia = (player.transform.position - transform.position);
 
+        if (distancia.magnitude < playerDistance && timer > waitTime) 
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            timer = 0;
+        }
     }
 
     void Spawner()
