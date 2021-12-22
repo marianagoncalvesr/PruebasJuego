55089b5c05b008f61db21b317115dd10ddfeba33
marianagoncalvesr <79268306+marianagoncalvesr@users.noreply.github.com>
2021-12-22 00:34:55 -0300

    portal lvl 2 linkeado

--- Juego/Assets/Script/Envivorment/PortalSwitch.cs
+++ Juego/Assets/Script/Envivorment/PortalSwitch.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 public class PortalSwitch : MonoBehaviour
 {
 
-    enum scenes {Intro = 1, Level1, LevelBoss1}
+    enum scenes {Intro = 1, Level1, LevelBoss1, Level2}
     [SerializeField] private scenes Change;
     [SerializeField] int kay = 0;
     [SerializeField] int diamonn = 0;
@@ -49,6 +49,9 @@ public class PortalSwitch : MonoBehaviour
             case scenes.LevelBoss1:
                 if (playerDiamond.diamonds == 20)
                     SceneManager.LoadScene("Level 1 Boss");
+                break;
+            case scenes.Level2:
+                    SceneManager.LoadScene("Level 2");
                 break;
             default:              
                 Debug.Log("No se puso una escena");
