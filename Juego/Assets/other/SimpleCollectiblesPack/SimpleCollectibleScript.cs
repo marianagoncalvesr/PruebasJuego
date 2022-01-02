40fa9f81366aca1ec8c275d8166f25e614f84169
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2022-01-02 19:30:44 -0300

    updated cambios juego con la llave

--- Juego/Assets/other/SimpleCollectiblesPack/SimpleCollectibleScript.cs
+++ Juego/Assets/other/SimpleCollectiblesPack/SimpleCollectibleScript.cs
@@ -3,39 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-public class SimpleCollectibleScript : MonoBehaviour {
+public class SimpleCollectibleScript : MonoBehaviour
+{
 
-	[SerializeField] int lvl;
+    [SerializeField] int lvl;
 
-	public bool rotate; // do you want it to rotate?
+    public bool rotate; // do you want it to rotate?
 
-	public float rotationSpeed;
+    public float rotationSpeed;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < GameManager.instance.DoorKeysTotal.Length; i++)
+        {
+            if(GameManager.instance.DoorKeysTotal[i].Lvl==lvl &&
+                GameManager.instance.DoorKeysTotal[i].Picked == true)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
 
-		if (rotate)
-			transform.Rotate (Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotate)
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+    }
 
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Collect();
+        }
+    }
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.tag == "Player") {
-			Collect ();
-		}
-	}
+    public void Collect()
+    {
+        this.gameObject.SetActive(false);
+        GameManager.instance.KeyPicked(lvl);
 
-	public void Collect()
-	{
-		this.gameObject.SetActive(false);
-		GameManager.instance.KeyPicked(lvl);
-
-	}
+    }
 }
