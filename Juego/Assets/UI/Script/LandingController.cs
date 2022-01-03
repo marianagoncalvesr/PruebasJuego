2dda00329ef0d90a604fffe6816d4cbc3724fc46
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2022-01-03 00:00:03 -0300

    fix

--- Juego/Assets/UI/Script/LandingController.cs
+++ Juego/Assets/UI/Script/LandingController.cs
@@ -1,28 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
 using UnityEngine.SceneManagement;
 
 public class LandingController : MonoBehaviour
 {
-    //[SerializeField] private InputField
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnClickPlay()
-    {
-        SceneManager.LoadScene("Nivel Actual");
-    }
 }
