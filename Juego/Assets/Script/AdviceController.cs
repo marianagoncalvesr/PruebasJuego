09e84894d33bb40e587028da4b1751bfbefdf05f
marianagoncalvesr <79268306+marianagoncalvesr@users.noreply.github.com>
2022-01-02 23:49:56 -0300

    cambios

--- Juego/Assets/Script/AdviceController.cs
+++ Juego/Assets/Script/AdviceController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AdviceController : MonoBehaviour
 {
+    private int distance = 40;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,26 @@ public class AdviceController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Warning();
+    }
+
+    private void Warning()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, distance))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Necesitas 3 llaves para abrir la puerta");
+                
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.forward * distance);
     }
 }
