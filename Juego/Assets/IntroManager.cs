1137ae78c178349699e0e5950adff25607c7e8f4
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2022-01-03 00:40:45 -0300

    fixeado dioamantes

--- Juego/Assets/IntroManager.cs
+++ Juego/Assets/IntroManager.cs
@@ -9,6 +9,7 @@ public class IntroManager : MonoBehaviour
     [SerializeField] GameObject portal3;
     void Start()
     {
+        DiamondController.cantidadDiamantes = 0;
         door = GameObject.FindGameObjectWithTag("Door");
         door.GetComponent<DoorController>().StartKeys(true);
         EnablePortals();
@@ -16,12 +17,12 @@ public class IntroManager : MonoBehaviour
 
     public void EnablePortals()
     {
-        if (GameManager.instance.GetKeysPicked() > 2)
+        if (GameManager.instance.GetKeysPicked() == 2)
         {
             portal2.SetActive(true);
             portal3.SetActive(true);
         }
-        else if (GameManager.instance.GetKeysPicked() > 1)
+        else if (GameManager.instance.GetKeysPicked() == 1)
         {
             portal2.SetActive(true);
         }
